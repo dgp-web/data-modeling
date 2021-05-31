eff08725c3dff5b18ba2fffd9ca712e192f897c8
dgp-web <dgp-web@outlook.de>
2021-05-31 09:24:31 +0200

    2.0.4 Adjust scale

--- src/lib/models/attribute-metadata-map.model.ts
+++ src/lib/models/attribute-metadata-map.model.ts
@@ -2,5 +2,5 @@ import { EntityTypeMap } from "./entity-management";
 import { AttributeMetadata } from "./attribute-metadata.model";
 
 export type AttributeMetadataMap<T, TEntityTypeMap extends EntityTypeMap> = {
-    [K in keyof T]?: AttributeMetadata<T[K], T[K]>
+    [K in keyof T]?: AttributeMetadata<T[K]>
 };
