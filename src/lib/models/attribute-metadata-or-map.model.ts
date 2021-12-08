8cb5d8acf3a55942109d7590051b0d51c0d1ea67
dgp-web <dgp-web@outlook.de>
2021-12-08 11:57:18 +0100

    Cache

--- src/lib/models/attribute-metadata-or-map.model.ts
+++ src/lib/models/attribute-metadata-or-map.model.ts
@@ -1,15 +1,50 @@
 import {AttributeMetadata} from "./attribute-metadata.model";
 import {ModelMetadata} from "./model-metadata.model";
+import {Many} from "./many.model";
 
-export type AttributeOrModelMetadata<TModel, TKey extends keyof TModel> = TModel[TKey] extends (infer E)[] ?
-    ModelMetadata<E, any>
-    : TModel[TKey] extends object
+export type ArrayType<T> = T[] | ReadonlyArray<T> | Many<T>;
+export type Flatten<Type> = Type extends Array<infer Item> ? Flatten<Item> : Type;
+
+export interface FlattenTest {
+    readonly one: string[];
+    readonly two: string[][];
+    readonly three: string[][][];
+}
+
+const test: FlattenTest = {
+    one: [], two: [], three: []
+};
+
+export type asd = Flatten<string[][][]>;
+
+export type AttributeOrModelMetadata<TModel, TKey extends keyof TModel> = TModel[TKey] extends ArrayType<(infer E)> ? // array or other
+    ModelMetadata<E, any> // TODO: We are finished here because we could encounter nested arrays
+    : TModel[TKey] extends object // object or primitive attribute
         ? ModelMetadata<TModel[TKey], any>
         : AttributeMetadata<TModel[TKey]>;
+/*
+export type AttributeOrModelMetadataMap<TModel> = {
+
+    readonly [TKey in keyof TModel]: TModel[TKey] extends ArrayType<(infer E)> ? // array or other
+        ModelMetadata<E, any> // TODO: We are finished here because we could encounter nested arrays
+        : TModel[TKey] extends object // object or primitive attribute
+            ? ModelMetadata<TModel[TKey], any>
+            : AttributeMetadata<TModel[TKey]>;
+
+}*/
 
 export type AttributeOrModelMetadataMap<TModel> = {
-    [K in keyof TModel]: AttributeOrModelMetadata<TModel, K>;
+
+    readonly [TKey in keyof TModel]: TModel[TKey] extends ArrayType<(infer E)> ? // array or other
+        Flatten<E> extends object
+            ? ModelMetadata<Flatten<E>, any>
+            : AttributeMetadata<Flatten<E>>// TODO: We are finished here because we could encounter nested arrays -->
+        : TModel[TKey] extends object // object or primitive attribute
+            ? ModelMetadata<TModel[TKey], any>
+            : AttributeMetadata<TModel[TKey]>;
+
 }
+
 
 export interface Nested {
     readonly sub: number;
@@ -18,13 +53,23 @@ export interface Nested {
 export interface TestModel {
     readonly label: string;
     readonly nested: Nested;
-    readonly nestedArray: Nested[];
+    readonly nestedArray: Array<Nested>;
+    readonly stringArray: string[];
 }
 
 const testModelMetadata: AttributeOrModelMetadataMap<TestModel> = {
-    label: {},
-    nested: {},
+    label: {
+
+    },
+    nested: {
+
+    },
     nestedArray: {
-        attributes: {}
+        attributes: {
+            sub: {}
+        }
+    },
+    stringArray: {
+
     }
 }
